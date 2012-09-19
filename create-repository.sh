97e489af3945353c5565c070afbe7a04f64f8e04
Fredrik Ekholdt <freekh@gmail.com>
2012-09-19 15:16:12 +0200

    0.2.0.1

--- create-repository.sh
+++ create-repository.sh
@@ -9,5 +9,7 @@ if ! [ -e $SBTLAUNCH ]; then
     curl http://typesafe.artifactoryonline.com/typesafe/ivy-releases/org.scala-sbt/sbt-launch/$SBTVERSION/sbt-launch.jar > $SBTLAUNCH
 fi
 
+#we set the commit here, because I could not figure out how to get sbt to pull the latest
 commit=`curl https://api.github.com/repos/freekh/deputy/commits | grep sha | head -n 2 | tail -n 1 | sed -e 's/.*sha\"\:[ ]*\"\(.*\)\".*/\1/'`
+echo "Latest commit:$commit"
 java -Ddeputy.commit=$commit -Xms512M -Xmx1536M -Xss1M -XX:+CMSClassUnloadingEnabled -XX:MaxPermSize=384M -jar $SBTLAUNCH ";project {git://github.com/freekh/deputy.git#$commit}; publish"
